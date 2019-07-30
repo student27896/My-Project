43450629c163ef400322c3423a7a8c830aef83e1
student17896 <53251403+student17896@users.noreply.github.com>
2019-07-30 14:23:59 +0800

    Update mymodule.py
    Changed the function name of get_weather() to get_city_weather().

--- mymodule.py
+++ mymodule.py
@@ -3,7 +3,7 @@ import json
 
 api_key = "xxxxxxx"
 
-def get_weather(city):
+def get_city_weather(city):
     sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" +
                            city + "&appid=" + api_key)
     result = sock.read()                            
@@ -11,16 +11,6 @@ def get_weather(city):
     weather = json.loads(result)
     return weather["main"]["temp"] -273.15
 
-def postal_lookup(postal_code):
-    sock = urlopen("http://api.postcodes.io/postcodes/" + postal_code)
-    result = sock.read()                            
-    sock.close()                                        
-    details = json.loads(result)
-    return (details["result"]["latitude"], details["result"]["longitude"])
-
 if __name__ == "__main__":
     degrees = get_weather("OSLO")
     print("Weather in Oslo is %.2f degrees Celsius" % degrees)
-
-    location = postal_lookup("B323PP")
-    print(location)
